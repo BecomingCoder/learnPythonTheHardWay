0ee17f72947e0f64d3c127f4b87cf6421a5091ca
Jason Scott <becomingcoder78@gmail.com>
2016-04-28 08:37:32 -0400

    Initial commit for Learn Python The Hard Way

---
+++ ex4.py
@@ -0,0 +1,24 @@
+# number of cars
+cars = 100
+# amount of space in a car
+space_in_a_car = 4.0
+# number of drivers
+drivers = 30
+# number of passengers
+passengers = 90
+# number of cars that dont have drivers
+cars_not_driven = cars - drivers
+# number of cars that have drivers
+cars_driven = drivers
+# amount of people that can travel
+carpool_capacity = cars_driven * space_in_a_car
+
+average_passengers_per_car = passengers / cars_driven
+
+
+print "There are", cars, "cars available."
+print "There are only", drivers, "drivers available."
+print "There will be", cars_not_driven, "empty cars today."
+print "We can transport", carpool_capacity, "people today."
+print "We have", passengers, "to carpool today."
+print "We need to put about", average_passengers_per_car, "in each car."
