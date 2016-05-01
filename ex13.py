c61498e7eec268dae3f92e271bc42cc3fa941ae7
Jason Scott <becomingcoder78@gmail.com>
2016-05-01 10:11:05 -0400

    Added exercise 13 & 14

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The scrpit is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+bonus = raw_input("Add something else here....")
